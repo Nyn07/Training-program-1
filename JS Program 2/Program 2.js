e931330f0b923ca95595d450b03fff12a5bbec27
Nayan Mehta <nayanmehta0709@gmail.com>
2022-11-25 11:25:58 +0530

    Add assignment for null and comments

---
+++ JS Program 2/Program 2.js
@@ -0,0 +1,16 @@
+/* 1 program of Node.js */
+name = 'Nayan Mehta' /* used const because it will never change */
+email = 'nayanmehta0709@gmail.com' /* let because it may change */
+age = '22' 
+address = '1437 B scheme no. 71' 
+phone_no = '9575260083' 
+gender = 'male'
+married = 'No'
+
+console.log ('Name =',name)
+console.log ('Email =',email)
+console.log ('Age =',age)
+console.log ('Address =',address)
+console.log ('Gender =',gender)
+console.log ('Phone Number =',phone_no)
+console.log ('Married =', married )
