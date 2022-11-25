07b82a3634bcabee1145d47e3474d6445170ae04
Nayan Mehta <nayanmehta0709@gmail.com>
2022-11-25 11:06:41 +0530

    Add assignment for variables

---
+++ JS Program 1/program.js
@@ -0,0 +1,15 @@
+const name = 'Nayan Mehta' 
+let email = 'nayanmehta0709@gmail.com'
+let age = 50
+let address = '1437 B scheme no. 71 Indore (M.P.)'
+let phoneNo = 9575260083
+const gender = 'male'
+let isMarried = no 
+
+console.log ('Name =',name)
+console.log ('Email =',email)
+console.log ('Age =',age)
+console.log ('Address =',address)
+console.log ('Gender =',gender)
+console.log ('Phone Number =',phoneNo)
+console.log ('Are you Married =', isMarried)
